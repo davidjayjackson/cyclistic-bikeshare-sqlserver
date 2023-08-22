47407c02faabcadd486844f3fd13ecbdabbadc91
davidjayjackson <46544155+davidjayjackson@users.noreply.github.com>
2023-08-21 22:27:36 -0400

    Added table for Top 10 station detail and summary

--- analytics_eda_etl.sql
+++ analytics_eda_etl.sql
@@ -126,3 +126,83 @@ INTO start_hour_distance
 	WHERE IsOutlier = 0
 	GROUP BY start_date,start_hour
 ORDER BY start_date,start_hour;
+
+-- Top 20 start stations
+
+-- Create a temporary table containing the top 20 start stations by row count
+WITH Top20Stations AS (
+    SELECT TOP 20 start_station_name
+    FROM analytics
+    GROUP BY start_station_name
+    ORDER BY COUNT(*) DESC
+)
+
+-- Create the new table containing the rows for the top 20 start stations
+SELECT a.*
+INTO top_start_stations  -- Replace 'new_table_name' with your desired table name
+FROM analytics a
+INNER JOIN Top20Stations t
+ON a.start_station_name = t.start_station_name;
+
+SELECT * FROM top_start_stations;
+
+-- Rank stations by row count
+WITH RankedStations AS (
+    SELECT 
+        start_station_name,
+        COUNT(*) as station_count,
+        RANK() OVER (ORDER BY COUNT(*) DESC) AS rank,
+        DENSE_RANK() OVER (ORDER BY COUNT(*) DESC) AS dense_rank
+    FROM analytics
+    GROUP BY start_station_name
+)
+
+-- Calculate the total rows in the analytics table
+, TotalRows AS (
+    SELECT COUNT(*) as total_count FROM analytics
+)
+
+-- Select the desired columns into the new table
+SELECT 
+    rs.start_station_name,
+    rs.station_count,
+    rs.rank,
+    rs.dense_rank,
+    CAST(ROUND((rs.station_count * 100.0 / tr.total_count), 2) AS DECIMAL(10, 2)) AS station_percentage
+INTO station_rank
+FROM RankedStations rs
+CROSS JOIN TotalRows tr
+WHERE rs.rank <21;
+
+
+
+DROP TABLE station_rank;
+SELECT * FROM station_rank;
+
+--- top 10 stations daily summary
+-- Identify the top 10 stations from station_rank
+WITH Top10Stations AS (
+    SELECT TOP 10 start_station_name
+    FROM station_rank
+    ORDER BY rank
+)
+
+-- Join with the analytics table and aggregate the results
+SELECT 
+    a.start_station_name,
+    a.start_date,
+    COUNT(*) as row_count,
+    SUM(a.trip_duration) as total_trip_duration,
+    AVG(a.trip_duration) as avg_trip_duration,
+    SUM(a.trip_distance) as total_trip_distance,
+    CAST(ROUND(AVG(a.trip_distance), 2) AS DECIMAL(10, 2)) as avg_trip_distance
+INTO new_analytics_summary
+FROM analytics a
+INNER JOIN Top10Stations t ON a.start_station_name = t.start_station_name
+GROUP BY a.start_station_name, a.start_date
+ORDER BY a.start_station_name, a.start_date;
+
+
+
+SELECT *FROM new_analytics_summary;
+DROP TABLE new_analytics_summary
